c5f8d7045e4459b1832162d423b7efb6b2d1b980
SmackNick <npalston@gmail.com>
2024-09-12 17:55:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById('my-form');
 
-    const submitForm = function(form) {
-        form.addEventListener("submit", function(e) {
-            e.preventDefault();
-            const formData = new FormData(form);
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();  // Prevents the form from refreshing the page
+        console.log("Form submitted");
 
-            formspree.submitForm('xanwzolk', formData)  // Replace 'your-form-id'
-                .then(response => {
-                    if (response.ok) {
-                        alert('Form submitted successfully!');
-                    } else {
-                        alert('Form submission failed.');
-                    }
-                })
-                .catch(error => {
-                    console.error('Error submitting form:', error);
-                });
-        });
-    };
+        const formData = new FormData(form);
 
-    submitForm(form);
+        formspree.submitForm('xanwzolk', formData)  // Replace 'your-form-id' with your actual Formspree form ID
+            .then(response => {
+                if (response.ok) {
+                    console.log('Form submitted successfully');
+                    alert('Form submitted successfully!');
+                } else {
+                    console.log('Error submitting form:', response);
+                    alert('Form submission failed.');
+                }
+            })
+            .catch(error => {
+                console.error('Error submitting form:', error);
+                alert('Error submitting form.');
+            });
+    });
 });
